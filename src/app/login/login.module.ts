48f4b24194bfc991739400f1ebd6dcf65aea2f3f
Guilherme <guilhermecc2@hotmail.com>
2023-04-25 17:09:12 -0300

    login screen

---
+++ src/app/login/login.module.ts
@@ -0,0 +1,22 @@
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { LoginPageRoutingModule } from './login-routing.module';
+
+import { LoginPage } from './login.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    LoginPageRoutingModule,
+    RouterModule
+  ],
+  declarations: [LoginPage]
+})
+export class LoginPageModule {}
